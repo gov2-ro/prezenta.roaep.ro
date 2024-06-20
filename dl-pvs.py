0b54b2ae228d86583ad43d23265ae0227c64428b
pax <pax@mioritics.ro>
2024-06-21 01:21:06 +0300

    parametrize pv type

--- dl-pvs.py
+++ dl-pvs.py
@@ -1,20 +1,43 @@
-csv_list='data/siruta-cod_jud.csv'
-output_dir = "data/pvs"
-log_file = "data/pv-dl.log"
-csv_col_siruta="uat.siruta"
-csv_col_jud="cc"
-# functie_aleasa='p'
-functie_aleasa='cl'
+functie_alesi='p'
+# functie_alesi='cl'
+# functie_alesi='cj'
+functie_alesi='pcj'
 
-output_dir += f"_{functie_aleasa}"
+pv_type='temp'
+pv_type='part'
+# pv_type='final'
+
+# uat_type='uat'
+uat_type='cnty'
+
+
+data_root = "data/"
+csvs={
+    'cnty': 'judete-siruta.csv',
+    'uat': 'siruta-cod_jud.csv'
+}
+csv_cols={
+    'cnty': 'cc',
+    'uat': 'siruta'
+}
+csv_list = data_root + csvs[uat_type]
+
+output_dir = data_root + "pvs/" + functie_alesi + "/" + uat_type + "/" + pv_type + "/"  
+log_file = data_root + "PVs-dl-" +  functie_alesi + "-" + uat_type + "-" + pv_type + ".log"
+# csv_col_siruta="uat.siruta"
+csv_cols_jud=csv_cols[uat_type]
+csv_col_jud="cc" #UATs
+csv_col_jud="Cod" #judete
+
+
+base_url = "https://prezenta.roaep.ro/locale09062024/data/csv/sicpv/pv_{}_{}_{}_{}.csv" 
+
+# output_dir += f"_{functie_alesi}"
 
 import pandas as pd
 import requests
 import os
 from tqdm import tqdm
-
-
-base_url = "https://prezenta.roaep.ro/locale09062024/data/csv/sicpv/pv_part_uat_{}_{}_{}.csv"
 
 
 df = pd.read_csv(csv_list)
@@ -24,11 +47,13 @@ if not os.path.exists(output_dir):
 
 with open(log_file, 'w') as log:
     for index, row in tqdm(df.iterrows(), total=len(df)):
-        url = base_url.format(functie_aleasa, row[csv_col_jud], row[csv_col_siruta])
+        url = base_url.format(pv_type, uat_type, functie_alesi, row[csv_col_jud].lower()) 
+
         try:
             response = requests.get(url)
             if response.status_code == 200:
-                file_path = os.path.join(output_dir, f"pv_part_uat_{functie_aleasa}_{row[csv_col_jud]}_{row[csv_col_siruta]}.csv")
+                # file_path = os.path.join(output_dir, f"pv_part_uat_{functie_alesi}_{row[csv_col_jud]}_{row[csv_col_siruta]}.csv") #UATs
+                file_path = os.path.join(output_dir, f"pv_{pv_type}_{uat_type}_{functie_alesi}_{row[csv_col_jud]}.csv") #județe
                 with open(file_path, 'wb') as f:
                     f.write(response.content)
                 log.write(f"Downloaded: {url}\n")
@@ -36,3 +61,5 @@ with open(log_file, 'w') as log:
                 log.write(f"Failed: {url} - Status code: {response.status_code}\n")
         except requests.ConnectionError as e:
             log.write(f"Connection error: {url} - Error: {e}\n")
+#
+print("Done! " + str(len(df)) + " files downloaded to: ", output_dir)
